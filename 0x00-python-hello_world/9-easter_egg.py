a436b7c7157f7639846a6a5da8cf072a6c4fead6
Yaqub <02omeiza@gmail.com>
2022-12-03 15:39:33 -0800

    ren python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,25 +1,2 @@
 #!/usr/bin/python3
-a = 'The Zen of Python, by Tim Peters'
-b = 'Beautiful is better than ugly.'
-c = 'Explicit is better than implicit.'
-d = 'Simple is better than complex.'
-e = 'Complex is better than complicated.'
-f = 'Flat is better than nested.'
-g = 'Sparse is better than dense.'
-h = 'Readability counts.'
-i = "Special cases aren't special enough to break the rules."
-j = 'Although practicality beats purity.'
-l = 'Errors should never pass silently.'
-m = 'Unless explicitly silenced.'
-n = 'In the face of ambiguity, refuse the temptation to guess.'
-o = 'There should be one-- and preferably only one --obvious way to do it.'
-p = "Although that way may not be obvious at first unless you're Dutch."
-q = 'Now is better than never.'
-r = 'Although never is often better than *right* now.'
-s = "If the implementation is hard to explain, it's a bad idea."
-t = 'If the implementation is easy to explain, it may be a good idea.'
-u = "Namespaces are one honking great idea -- let's do more of those!"
-zen = b + '\n' + c + '\n' + d + '\n' + e + '\n' + f + '\n' + g + '\n' + h + '\n' + i + '\n' + j + '\n' + l + '\n'+ m + '\n' + n + '\n' + o + '\n' + p + '\n' + q  + '\n' + r + '\n' + s + '\n' + t + '\n' + u
-print(a)
-print('\n')
-print(zen)
+import thiswq
