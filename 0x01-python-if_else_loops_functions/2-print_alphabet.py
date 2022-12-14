7afe10d851ccc2bd0cd21809273b6a5db268b7fc
Yaqub <02omeiza@gmail.com>
2022-12-14 04:34:07 -0800

    pyn elif

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#this program is to print the ASCII alphabet in lowercase
 for alplow in range(97, 123):
-    print("{}".format(chr(alplow)), end = "")
+    print("{}".format(chr(alplow)), end="")
